3656e8d3b611f5284349e56c89cb9e2f1a418141
l7916213913 <43435676+l7916213913@users.noreply.github.com>
2019-04-15 06:53:26 +0800

    Update ball.py

--- ball.py
+++ ball.py
@@ -4,26 +4,34 @@ import imutils
 import time
 import serial
 
+i=0
 timef =15 
+##time modulus parameter
 tennislower = (29, 86, 6)
 tennisupper = (64, 255, 255)
-i=0
+##color scale
 vs = cv2.VideoCapture(0)
+##catch the pi camera
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout = 1)
+##read Arduino signal from usb hub
 while (vs.isOpened()):
     ret, frame = vs.read()
-        
+##take 1 picture every 15 frame 
     if (i % timef == 0):
         fn = time.strftime('a')+'.jpg'
         cv2.imwrite(fn,frame)
         img = cv2.imread('a.jpg')
+        
+        #image prefix (change to single-channel, erode, dilate)
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
         mask =cv2.inRange(hsv,tennislower, tennisupper)
         mask = cv2.erode(mask, None, iterations=2)
         mask = cv2.dilate(mask, None, iterations=2)
+        
+        #find contours and grab them
         cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         cnts = imutils.grab_contours(cnts)
-
+        #
         if len(cnts) > 0:
             c = max(cnts, key=cv2.contourArea)
             M = cv2.moments(c)
